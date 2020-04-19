2c4cee95fa27a2c71bb1fb626122582c6f758cea
Thomas <samoht.t@hotmail.fr>
2020-04-19 17:52:08 +0200

    Updated dependencies

--- public/electron.js
+++ public/electron.js
@@ -1,28 +1,33 @@
 const electron = require('electron')
-const {
-    app,
-    BrowserWindow
-} = electron
+const { app, BrowserWindow } = electron
 const isDev = require('electron-is-dev')
 const path = require('path')
 let mainWindow
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 900,
-        height: 680
-    });
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
-    mainWindow.on('closed', () => mainWindow = null)
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+  })
+  mainWindow.setMenuBarVisibility(isDev)
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  mainWindow.on('closed', () => (mainWindow = null))
 }
-app.on('ready', createWindow);
+
+app.on('ready', createWindow)
+
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow()
-    }
-});
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
